bf7ba556f1b571fc57d4f1f458995de6d944842b
Tobias Heim <tobias.heim@outlook.com>
2022-08-26 11:23:57 +0200

    Updated the get content part

--- pull-transcripts.py
+++ pull-transcripts.py
@@ -20,13 +20,25 @@ if __name__ == "__main__":
         "Accept" : "application/json"      
     }  
 
+    # Get meeting id from the meeting url
     res = requests.get(f"https://graph.microsoft.com/beta/users/{os.environ['USER_ID']}/onlineMeetings?$filter=JoinWebUrl%20eq%20'{os.environ['MEETING_URL']}'", headers=headers)
     online_meeting_id = res.json()['value'][0]['id']
 
+    # Get transcript id from the meeting id
     transcript_res = requests.get(f"https://graph.microsoft.com/beta/users/{os.environ['USER_ID']}/onlineMeetings/{online_meeting_id}/transcripts", headers=headers)
     transcript_id = transcript_res.json()['value'][0]['id']
 
-    transcript_content_res = requests.get(f"https://graph.microsoft.com/beta/users/{os.environ['USER_ID']}/onlineMeetings/{online_meeting_id}/transcripts{transcript_id}/content", headers=headers)
-    transcript_content_data = transcript_content_res.json()['value']
+    content_headers = {
+        "Authorization" : f"Bearer {access_Token}",
+        "Content-Type" : "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        "Accept" : "application/json"      
+    } 
 
-    print(transcript_content_data)
+    # Get transcript content
+    transcript_content_res = requests.get(f"https://graph.microsoft.com/beta/users/{os.environ['USER_ID']}/onlineMeetings/{online_meeting_id}/transcripts/{transcript_id}/content?$format=text/vtt", headers=content_headers)
+    
+    # Convert transcript content
+    decoded_response = transcript_content_res.content.decode("utf-8")
+    
+    # Print transcript content
+    print(decoded_response)
