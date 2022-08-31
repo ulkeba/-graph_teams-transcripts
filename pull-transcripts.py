bbc855aa6a31de43ec94e8268a808dac87923998
Bastian Ulke <bastian.ulke@microsoft.com>
2022-08-31 19:51:17 +0200

    Use meeting id instead of user id and meeting url as input.

--- pull-transcripts.py
+++ pull-transcripts.py
@@ -19,14 +19,24 @@ if __name__ == "__main__":
         "Content-Type" : "application/json",
         "Accept" : "application/json"      
     }  
+    
+    meeting_record = requests.get(f"https://graph.microsoft.com/beta/communications/callRecords/{os.environ['MEETING_ID']}", headers=headers)
+    
+    organizer_id = meeting_record.json()['organizer']['user']['id']
+    meeting_url = meeting_record.json()['joinWebUrl']
 
-    # Get meeting id from the meeting url
-    res = requests.get(f"https://graph.microsoft.com/beta/users/{os.environ['USER_ID']}/onlineMeetings?$filter=JoinWebUrl%20eq%20'{os.environ['MEETING_URL']}'", headers=headers)
+    print(f"organizer_id :  {organizer_id}")
+    print(f"meeting_url :  {meeting_url}")
+
+    res = requests.get(f"https://graph.microsoft.com/beta/users/{organizer_id}/onlineMeetings?$filter=JoinWebUrl%20eq%20'{meeting_url}'", headers=headers)
+
     online_meeting_id = res.json()['value'][0]['id']
+    print(f"online_meeting_id :  {online_meeting_id}")
 
     # Get transcript id from the meeting id
-    transcript_res = requests.get(f"https://graph.microsoft.com/beta/users/{os.environ['USER_ID']}/onlineMeetings/{online_meeting_id}/transcripts", headers=headers)
+    transcript_res = requests.get(f"https://graph.microsoft.com/beta/users/{organizer_id}/onlineMeetings/{online_meeting_id}/transcripts", headers=headers)
     transcript_id = transcript_res.json()['value'][0]['id']
+    print(f"transcript_id :  {transcript_id}")
 
     content_headers = {
         "Authorization" : f"Bearer {access_Token}",
@@ -35,10 +45,11 @@ if __name__ == "__main__":
     } 
 
     # Get transcript content
-    transcript_content_res = requests.get(f"https://graph.microsoft.com/beta/users/{os.environ['USER_ID']}/onlineMeetings/{online_meeting_id}/transcripts/{transcript_id}/content?$format=text/vtt", headers=content_headers)
+    transcript_content_res = requests.get(f"https://graph.microsoft.com/beta/users/{organizer_id}/onlineMeetings/{online_meeting_id}/transcripts/{transcript_id}/content?$format=text/vtt", headers=content_headers)
     
     # Convert transcript content
     decoded_response = transcript_content_res.content.decode("utf-8")
     
     # Print transcript content
+    print(f"Transcript Content:")
     print(decoded_response)
